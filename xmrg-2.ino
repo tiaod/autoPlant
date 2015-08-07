3a1eae013470716cac654f6ca59ad50c1b41853f
xmrg <tiaod@qq.com>
2015-08-07 21:33:19 +0800

    修改一些参数

--- xmrg-2.ino
+++ xmrg-2.ino
@@ -3,10 +3,10 @@
 #include <Time.h>
 #include <TimeAlarms.h>
 ///////////////////////custom settings///////////////////////////////////////
-#define CHECK_INTERVAL 15 //检查土壤湿度间隔的秒数(三小时一次)
+#define CHECK_INTERVAL 9600 //检查土壤湿度间隔的秒数(三小时一次)
 #define MAX_SOIL_HUMIDITY 600 //土壤的湿度阈值
 #define WATER_TIME 5 //单次浇水的秒数
-#define IS_MINIMUN_POWER true //是否开启节能模式。用于太阳能电池供电的情况
+#define IS_MINIMUN_POWER false //是否开启节能模式。用于太阳能电池供电的情况
 
 ///////////////////////ping define///////////////////////////////////////////
 #define RELAY1 9    //继电器1,节能模式时控制其他模块的电源
@@ -20,9 +20,11 @@ LiquidCrystal lcd(8, 7, 6, 5, 4, 3);//初始化LCD
 
 void check_soid_humidity(){
   digitalWrite(RELAY1, HIGH);
+  delay(100);
   Serial.print(F("Check soil humidity....."));
   lcd.setCursor(0,0);
   lcd.print("Checking soil...");
+  Serial.print(analogRead(SOIL_HUMIDITY_PIN));
   for(int i =0;i <= 5;i++){//重复检查5次，如果5次都是干旱，则确定要浇水
     if(analogRead(SOIL_HUMIDITY_PIN)<MAX_SOIL_HUMIDITY){
       Serial.println(F("done"));
@@ -31,7 +33,12 @@ void check_soid_humidity(){
     else{
       digitalDHT11Display();
       Serial.print(F("dry, "));
-      delay(10000);//间隔10秒
+      if(IS_MINIMUN_POWER){//如果已经开启了节能模式
+        delay(100);//快速节约时间
+      }
+      else{
+        delay(1000 );//间隔10秒
+      }
     }
   }
   Serial.println();
@@ -40,6 +47,9 @@ void check_soid_humidity(){
   delay(WATER_TIME*1000);
   digitalWrite(RELAY2, LOW);  //进行浇水操作
   Serial.println(F("done"));
+  if(IS_MINIMUN_POWER){//如果已经开启了节能模式
+    digitalWrite(RELAY1, LOW);//节能
+  }
   return;
 }
 
@@ -64,7 +74,7 @@ void digitalRuntimeDisplay(){
 void digitalDHT11Display(){//只有常规状态才读取温度并且显示，防止卡顿
   lcd.setCursor(0, 1);
   if(digitalRead(RELAY1)==0){
-    lcd.print("                ");
+    lcd.print("break                ");
     return;//退出
   }
   float t = dht.readTemperature();
@@ -85,13 +95,12 @@ void digitalDHT11Display(){//只有常规状态才读取温度并且显示，防
 }
 
 void setup() {
-  if(!IS_MINIMUN_POWER){
-    digitalWrite(RELAY1, HIGH);
-  }
+
   Serial.begin(9600);
   Serial.print(F("init..."));
   lcd.begin(16, 2);
   lcd.print(F("hello, xmrg!"));
+
   //////////////////////////////DHT11////////////////////////////////////
    
   dht.begin();//DHT11
@@ -101,7 +110,10 @@ void setup() {
   pinMode(RELAY2, OUTPUT);
   Alarm.timerRepeat(CHECK_INTERVAL, check_soid_humidity); //每间隔一段时间检查一次
   Alarm.timerRepeat(60, digitalDHT11Display); //每分钟更新一次温湿度
-  
+  if(!IS_MINIMUN_POWER){
+    digitalWrite(RELAY1, HIGH);
+    digitalDHT11Display();
+  }
   Serial.println(F("done."));
 }
 
